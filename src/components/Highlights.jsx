2e263629f1a945ccc6cecf509f4bd6a5d73ab705
ali-amirasgari <aliamirasgari2002@gmail.com>
2024-09-01 16:57:30 +0330

    Create navbar , hero , highlight component and config the project

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,60 @@
+import { watchImg, rightImg } from "@/utils"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
+
+const Highlights = () => {
+
+    useGSAP(() => {
+        gsap.to('#title', {
+            scrollTrigger: {
+                trigger: '#title',
+                start: "bottom bottom",
+                end: "top top",
+                scrub: true
+            },
+            opacity: 1,
+            y: 0
+        })
+        gsap.to('.link', {
+            scrollTrigger: {
+                trigger: '.link',
+                start: "bottom bottom",
+                end: "top top",
+                scrub: true
+            },
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            stagger: 0.25
+        })
+    }, [])
+
+    return (
+        <section
+            id="highlights"
+            className="w-screen overflow-hidden h-full common-padding bg-zinc"
+        >
+            <div className="screen-max-width">
+                <div className="mb-12 w-full md:flex items-end justify-between">
+                    <h1 id="title" className="section-heading">Get the highlights.</h1>
+
+                    <div className="flex flex-wrap items-end gap-5">
+                        <p className="link">
+                            Watch the film
+                            <img src={watchImg} alt="watch" className="ml-2" />
+                        </p>
+                        <p className="link">
+                            Watch the event
+                            <img src={rightImg} alt="right" className="ml-2" />
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Highlights
